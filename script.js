bc4f53fcef0bd15874ba0063129bf105c93f44d3
Luiz <lhvalentim@outlook.coom>
2023-01-06 14:56:58 -0300

    test

--- script.js
+++ script.js
@@ -12,19 +12,52 @@ const options = {
 		name: 'lorem',
 		displayName: 'lorem'
 	},
-	authenticatorSelection: {
+	/* authenticatorSelection: {
 		authenticatorAttachment: 'platform'
-	},
+	}, */
 	pubKeyCredParams: [
 		{type: 'public-key', alg: -7}
 	],
+	timeout: 60000,
 	attestation: 'direct'
 }
 
 document.querySelector('button:first-of-type').onclick = () => {
 	navigator.credentials.create({publicKey: options})
 	.then(credential => {
-		navigator.credentials.store(credential)
+		navigator.credentials.get({publicKey: {
+			challenge: encoder.encode('challange'),
+			allowCredentials: [
+				{
+					id: credential.rawId,
+					transports: ['usb', 'nfc', 'ble', 'internal'],
+					type: 'public-key'
+				}
+			]
+		}})
+		.then(response => {
+			console.log(response)
+		})
+	})
+	.catch(error => {
+		console.log(error)
+	})
+}
+
+document.querySelector('button:nth-of-type(2)').onclick = () => {
+	navigator.credentials.get({publicKey: {challenge: encoder.encode('challange')}})
+	.then(credential => {
+		alert(`Seu ID: ${credential.id}`)
+	})
+	.catch(error => {
+		console.log(error)
+	})
+}
+
+document.querySelector('button:nth-of-type(3)').onclick = () => {
+	credential = new FederatedCredential({id: 1, name: 'lorem', provider: location.host})
+	navigator.credentials.store(credential)
+	.then(() => {
 		console.log('Success')
 	})
 	.catch(error => {
@@ -32,8 +65,8 @@ document.querySelector('button:first-of-type').onclick = () => {
 	})
 }
 
-document.querySelector('button:last-of-type').onclick = () => {
-	navigator.credentials.get({publicKey: {challenge: encoder.encode('challange')}})
+document.querySelector('button:nth-of-type(4)').onclick = () => {
+	navigator.credentials.get('conditional')
 	.then(credential => {
 		alert(`Seu ID: ${credential.id}`)
 	})
