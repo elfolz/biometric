26d82959e2e68baffbe87c7693e58591cf0ebf6f
Luiz <lhvalentim@outlook.coom>
2023-01-06 12:37:48 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const encoder = new TextEncoder()
+
+const challange = encoder.encode('lorem')
+
+const publicKey = {
+	challange: challange,
+	rp: {
+		name: 'lorem'
+	},
+	user: {
+		id: '123456'
+	},
+	pubKeyCredParams: [
+		{
+			type: 'public-key',
+			alg: -7
+		}
+	]
+}
+
+navigator.credentials.create(publicKey)
+.then(response => {
+	console.log(response)
+})
+.catch(error => {
+	console.log(error)
+})
